d9b5de2f585609ec46c98c93962e4d617d4a02d1
lcamilo <lhenriquegomescamilo@gmail.com>
2022-02-27 23:12:50 +0000

    :art: created map if true operators.

---
+++ src/map/mapIfTrue.rx.ts
@@ -0,0 +1,15 @@
+import {map, Observable} from "rxjs";
+
+export function mapIfTrue<T, R>(
+    predicate: (t: T) => boolean,
+    ifTrue: (t: T) => R
+): (s: Observable<T>) => Observable<T | R> {
+    return (source: Observable<T>): Observable<T | R> => {
+        return source.pipe(map((value: T) => {
+            if (predicate(value)) {
+                return ifTrue(value);
+            }
+            return value;
+        }))
+    }
+}
---
+++ test/map.spec.ts
@@ -0,0 +1,35 @@
+import { of } from "rxjs";
+import { mapIfTrue } from "../src/map/mapIfTrue.rx";
+
+
+test("it should map value if predicate is true", done => {
+    const acc = [];
+    of({ v: 1 }, { v: 2 })
+        .pipe(mapIfTrue(
+            ({ v }) => v > 1,
+            ({ v }) => ({ m: (v * 2) })
+        ))
+        .subscribe({
+            next: value => acc.push(value),
+            complete: () => {
+                expect(acc).toEqual([{ v: 1 }, { m: 4 }]);
+                done();
+            }
+        });
+});
+
+test("it should not map value if predicate is false", done => {
+    const acc = [];
+    of({ v: 1 }, { v: 2 })
+        .pipe(mapIfTrue(
+            ({ v }) => v < 0,
+            ({ v }) => ({ v: 0 })
+        ))
+        .subscribe({
+            next: value => acc.push(value),
+            complete: () => {
+                expect(acc).toEqual([{ v: 1 }, { v: 2 }]);
+                done();
+            }
+        });
+});
