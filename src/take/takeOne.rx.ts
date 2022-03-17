b413f52f643f3272ea66542d0a5b4206a69c2abc
lcamilo <lhenriquegomescamilo@gmail.com>
2022-03-17 22:48:09 +0000

    :art: feat(takeOne): Fixed lint

--- src/take/takeOne.rx.ts
+++ src/take/takeOne.rx.ts
@@ -3,6 +3,6 @@ import { Observable, take } from "rxjs";
 
 export function takeOne<T>() {
     return (source: Observable<T>): Observable<T> => {
-        return source.pipe(take(1))
+        return source.pipe(take(1));
     }
 }
